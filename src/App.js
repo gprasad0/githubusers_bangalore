3c01433394bdfa804276619bcc56f8c6ee295088
guru <guru@foodmonk.com>
2019-05-12 10:10:05 +0530

    modifications

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ImageCards from "./components/ImageCards";
+import SearchBar from "./components/SearchBar";
+import axios from 'axios';
+import "./components/gitUsers.css";
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state={
+    data:[],
+    isLoading:true
+  }
+  userSearchTerm = async (value,value1) => {
+console.log("--------->",value,value1);
+
+await axios.get(`https://api.github.com/search/users?q=${value}+location:Bangalore&page=${value1}`)
+.then(response =>{
+console.log("data",response.data);
+this.setState({
+  data:response.data.items,
+  isLoading:false
+})
+})
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar userSearchTerm={this.userSearchTerm} />
+        {!this.state.isLoading?( <ImageCards data={this.state.data}/>):""}
+       
+      </div>
+    );
+  }
 }
 
 export default App;
